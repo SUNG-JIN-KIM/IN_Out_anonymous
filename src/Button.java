1d15176dcd04b8ac159cfd95f17514d1770f6613
SUNG-JIN-KIM <ksjcloud98@gmail.com>
2025-06-16 13:46:14 +0900

    Button Commit

---
+++ src/Button.java
@@ -0,0 +1,33 @@
+public class Button {
+    interface OnClickListener{
+        void onClick();
+    }
+
+    private OnClickListener listener;
+
+    public void setOnClicklistener(OnClickListener listener){
+        this.listener = listener;
+    }
+
+    public void click(){
+        if(listener != null){
+            listener.onClick();
+        }
+    }
+
+    private class ClickHandler implements OnClickListener{
+        @Override
+        public void onClick(){
+            System.out.println("Button was clicked!");
+        }
+    }
+
+    public void simulate(){
+        setOnClicklistener(new ClickHandler());
+        click();
+    }
+    public static void main(String[] args) {
+        Button btn = new Button();
+        btn.simulate();
+    }
+}
